5d6626b919fe11e3501e2c3055f97765edc59abe
Elias-m-S <esehmsdorf68@gmail.com>
2026-01-22 08:36:29 +0100

    Add files via upload

---
+++ CubeMx/Core/Inc/main.h
@@ -0,0 +1,105 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Input_Irq_Pin GPIO_PIN_13
+#define B1_Input_Irq_GPIO_Port GPIOC
+#define B1_Input_Irq_EXTI_IRQn EXTI15_10_IRQn
+#define SEG_F_Pin GPIO_PIN_0
+#define SEG_F_GPIO_Port GPIOC
+#define SEG_G_Pin GPIO_PIN_1
+#define SEG_G_GPIO_Port GPIOC
+#define R1_ADC_INPUT_Pin GPIO_PIN_0
+#define R1_ADC_INPUT_GPIO_Port GPIOA
+#define R2_ADC_INPUT_Pin GPIO_PIN_1
+#define R2_ADC_INPUT_GPIO_Port GPIOA
+#define SEG_A_Pin GPIO_PIN_5
+#define SEG_A_GPIO_Port GPIOA
+#define SEG_B_Pin GPIO_PIN_6
+#define SEG_B_GPIO_Port GPIOA
+#define SEG_C_Pin GPIO_PIN_7
+#define SEG_C_GPIO_Port GPIOA
+#define SEG_COM_Pin GPIO_PIN_0
+#define SEG_COM_GPIO_Port GPIOB
+#define LED_D1_Pin GPIO_PIN_10
+#define LED_D1_GPIO_Port GPIOB
+#define LED_D2_Pin GPIO_PIN_7
+#define LED_D2_GPIO_Port GPIOC
+#define SEG_D_Pin GPIO_PIN_8
+#define SEG_D_GPIO_Port GPIOA
+#define SEG_E_Pin GPIO_PIN_9
+#define SEG_E_GPIO_Port GPIOA
+#define SW1_Input_Irq_Pin GPIO_PIN_10
+#define SW1_Input_Irq_GPIO_Port GPIOA
+#define SW1_Input_Irq_EXTI_IRQn EXTI15_10_IRQn
+#define LED_D0_Pin GPIO_PIN_5
+#define LED_D0_GPIO_Port GPIOB
+#define LED_D4_Pin GPIO_PIN_8
+#define LED_D4_GPIO_Port GPIOB
+#define LED_D3_Pin GPIO_PIN_9
+#define LED_D3_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
